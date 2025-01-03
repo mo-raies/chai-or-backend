cf6bf19e08d37c6718ac584e7832d1fb17dc3604
mo-raies <raiesmohammadhindoli0786@gmail.com>
2025-01-03 16:26:56 +0530

    refreshToken , auth.middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express"
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser ,loginUser,logoutUser} from "../controllers/user.controllers.js";
 import { upload} from "../middleware/multer.middleware.js"
+import {verifyJWT} from "../middleware/auth.middleware"
 
 
 const router = Router ()
@@ -19,6 +20,9 @@ router.route("/register").post(
   registerUser
 );
 
+router.route("/login").post(loginUser)
+// secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router 
 
